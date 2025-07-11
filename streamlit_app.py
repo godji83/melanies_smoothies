54d685d9a775039bee8d18ba6cc4a39801a23066
godji83 <57142792+godji83@users.noreply.github.com>
2025-07-11 13:45:39 +0000

    add watermelon in ingredients block

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
-#New section to display smoothiefroot nutrition information
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 # Write directly to the app
 st.title(f":cup_with_straw: Customise Your Smoothie!:cup_with_straw:")
 st.write(
@@ -30,7 +26,9 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-    
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
     # st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
